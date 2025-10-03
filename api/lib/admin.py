7bd91ad9414d0f6f8c525ab4946e7670bbaff329
Yoshioka <sss.yoshioka@gmail.com>
2025-10-04 01:00:08 +0900

    🎸 feat: 管理画面に Recipe 追加

--- api/lib/admin.py
+++ api/lib/admin.py
@@ -1,1 +1,14 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Recipe
+
+DEFAULT_READONLY_FIELDS = [
+    "created_at",
+    "updated_at",
+]
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ["id", "title"]
+    readonly_fields = DEFAULT_READONLY_FIELDS
